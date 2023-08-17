0e7766069d2a973f323b4b2ece2ada0655097a98
AC <a.chaurasia@sap.com>
2023-08-17 21:22:29 +0200

    cleanup ♻️

--- srv/shared/Config.js
+++ srv/shared/Config.js
@@ -1,13 +1,8 @@
 import dotenv from "dotenv";
-import config from "./Config.js";
 
 class Config {
   constructor() {
     dotenv.config();
-  }
-
-  get databaseUrl() {
-    return process.env.DATABASE_URL;
   }
 
   get logLevel() {
