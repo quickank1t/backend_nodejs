033a78db1bb66a631af03cdc46856962a744b549
AC <a.chaurasia@sap.com>
2023-08-18 23:03:50 +0200

    logger

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 import "dotenv/config";
 import Config from "./srv/shared/Config.js";
+import logger from "./srv/shared/LogUtils.js";
 import app from "./srv/app.js";
 
 app.listen(Config.port, () => {
-  console.log(`App listening on port ${Config.port}!`);
+  logger.info(`App listening on port ${Config.port}!`);
 });
