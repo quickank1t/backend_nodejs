9f3bcc9be9e50ec237940d57a94c2900016bffcb
AC <a.chaurasia@sap.com>
2023-08-19 11:15:01 +0200

    refactor ♻️

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import "dotenv/config";
-import Config from "./srv/shared/Config.js";
-import logger from "./srv/shared/LogUtils.js";
-import app from "./srv/app.js";
+import Config from "./server/shared/Config.js";
+import logger from "./server/shared/LogUtils.js";
+import app from "./server/app.js";
 
 app.listen(Config.port, () => {
   logger.info(`App listening on port ${Config.port}!`);
